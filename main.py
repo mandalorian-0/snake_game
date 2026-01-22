78cae0d3735c94f235dd095fbb1741c314ef71f1
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-22 00:08:06 -0800

    Setup screen dimension and create snake body

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle as t
+
+# setup screen dimensions and color
+screen = t.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+
+    new_segment = t.Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+screen.mainloop()
